ef6c495f94ded426533c6f7ed0ce245ff2ed6617
Libert Sin <majesticer13@gmail.com>
2019-03-24 10:40:50 +0000

    Update raw_jpg_sort.sh

--- raw_jpg_sort.sh
+++ raw_jpg_sort.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-echo "#### 이 프로그램의 저작권은 ⓒLibert Sin 에 있습니다. ####\n 자유로운 배포·수정은 허용하지만 원저작자와 출처를 표시해야 합니다.\n http://photoguraphy.com/\n#############\n\n\n"
+echo -e "#### 이 프로그램의 저작권은 ⓒLibert Sin 에 있습니다. ####\n 자유로운 배포·수정은 허용하지만 원저작자와 출처를 표시해야 합니다.\n http://photoguraphy.com/\n#############\n\n\n"
 
 mkdir raw
 mkdir jpg
